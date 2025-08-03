f95cc7f21a17ce812bb0dc9114352bb5c32190ff
sumit-dey-69 <sumitdey.dev@gmail.com>
2025-08-03 13:30:27 +0530

    Refactor layout in Home component for improved spacing and responsiveness

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,16 +8,16 @@ export default function Home() {
     <div>
       <Navbar />
       <GridBackground>
-        <div className="space-y-[0.25em] z-20 flex flex-col px-4 sm:px-6 md:px-8">
-          <p className="-rotate-12 text-[0.8em] sm:text-[1.05em] my-[2em] sm:my-[3.5em]">Hi, I&apos;m</p>
+        <div className="z-20 flex flex-col px-4 sm:px-6 md:px-8 md:mb-15">
+          <p className="-rotate-12 text-[0.9em] sm:text-[1.05em] my-[2.5em]">Hi, I&apos;m</p>
           <p className="font-pangaia space-x-2 sm:space-x-4 font-bold text-7xl sm:text-6xl md:text-8xl text-[#ff6200] mx-auto text-center leading-tight">
             <span>Sumit</span>
             <span>Dey</span>
           </p>
-          <div className="flex flex-col gap-[0.5em] items-center justify-center">
+          <div className="flex flex-col gap-[0.5em] md:text-sm items-center justify-center">
             <p className="flex flex-col text-[1em] sm:text-[1.35em] items-center hover:transition-all w-full text-center px-4">
               <span>Specialized in building fast, responsive web apps</span>
-              <span className="font-serif text-lg sm:text-base">
+              <span className="font-serif text-lg sm:text-base md:text-[0.95em]">
                 with ReactJS, NextJS & Tailwind CSS
               </span>
             </p>
